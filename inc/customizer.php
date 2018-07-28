63fd7e6f2f76e981462b0392b8cbc53b0e23d61e
Mark Howells-Mead <permanent.tourisg@gmail.com>
2018-07-28 17:09:45 +0200

    Initial code

---
+++ inc/customizer.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Hoth Theme Customizer
+ *
+ * @package Hoth
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function mhm_hoth_customize_register($wp_customize)
+{
+	$wp_customize->get_setting('blogname')->transport = 'postMessage';
+	$wp_customize->get_setting('blogdescription')->transport = 'postMessage';
+	$wp_customize->get_setting('header_textcolor')->transport = 'postMessage';
+
+	if (isset($wp_customize->selective_refresh)) {
+		$wp_customize->selective_refresh->add_partial('blogname', array(
+			'selector' => '.site-title a',
+			'render_callback' => 'mhm_hoth_customize_partial_blogname',
+		));
+		$wp_customize->selective_refresh->add_partial('blogdescription', array(
+			'selector' => '.site-description',
+			'render_callback' => 'mhm_hoth_customize_partial_blogdescription',
+		));
+	}
+}
+add_action('customize_register', 'mhm_hoth_customize_register');
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function mhm_hoth_customize_partial_blogname()
+{
+	bloginfo('name');
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function mhm_hoth_customize_partial_blogdescription()
+{
+	bloginfo('description');
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function mhm_hoth_customize_preview_js()
+{
+	$theme = wp_get_theme();
+	wp_enqueue_script('mhm-hoth-customizer', get_template_directory_uri() . '/assets/dist/scripts/customizer.min.js', array( 'customize-preview' ), $theme->Version, true);
+}
+add_action('customize_preview_init', 'mhm_hoth_customize_preview_js');
